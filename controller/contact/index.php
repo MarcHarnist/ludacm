cc5883783268d03427edcf27a25130631f29ec03
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2017-08-31 11:22:14 +0200

    Cleaning
    Cleaning useless comments
    Marc

--- controller/contact/index.php
+++ controller/contact/index.php
@@ -1,13 +1,3 @@
 <?php
-// On demande toutes les questions (modèle)
-// Cours OpenClassRoom sur OOP
-// Hey Mattew! Why didn't you construct an Object with question????
-
-// include_once("model/$dir/get_question.php"); // include model/ludacm/get_question.php
-// $question_save = get_question(0,500);//500 = all questions !
-
-// On effectue du traitement sur les données (contrôleur)
-// Ici, on doit surtout sécuriser l'affichage
-
-// On affiche la page (vue)
+//Display the view of contact page
 include_once("$www_real_path/view/contact/index.php");
