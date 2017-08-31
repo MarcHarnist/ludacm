a3cc17f481b7101677db73ebe4ac8dee4392503b
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2017-08-31 11:12:42 +0200

    Last changes
    Simple

--- controller/index.php
+++ controller/index.php
@@ -1,15 +1,16 @@
-<?php
 /**
 *  Hy,
 *  Trying to show the MVC of my project. I'll need several days to install all files.  
+*  This fill is installed in "controller"  directory
 *  Thanks for all reviews.
+*  Marc Harnist 2017/08/31
 */
-// Asking all questions in /model/ ->data base mysql
-// Cours OpenClassRoom - OOP
-// We can construct an Object with questions
-include_once("model/$dir/get_question.php"); // include model/ludacm/get_question.php
+
+include_once("model/game/get_question.php"); // include model/ludacm/get_question.php
 $question_save = get_question(0,500);//500 = all questions !
-// Datas treatment. (Controller) and securing the display
+
+// On effectue du traitement sur les données (contrôleur)
+// Ici, on doit surtout sécuriser l'affichage
 foreach($question_save as $cle => $question)
 {
     $question_save[$cle]['id'] = nl2br(htmlspecialchars($question['id']));
@@ -18,5 +19,7 @@ foreach($question_save as $cle => $question)
     $question_save[$cle]['faux'] = htmlspecialchars($question['faux']);
     $question_save[$cle]['faux_bis'] = htmlspecialchars($question['faux_bis']);
 }
-// Displaying page (view)
+
+// On affiche la page (vue)
 include_once("$www_real_path/view/game/game.php");
+
