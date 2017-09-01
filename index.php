2c45a10dfdce5b894f633f7ae30e81c8a66a7a24
MarcHarnist <31386370+MarcHarnist@users.noreply.github.com>
2017-09-01 08:53:07 +0200

    Code in files in model repertory
    Create files in model directory
    
    I create a file for localhost realpath and one for htaccess presence control

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <?php
-$www_real_path = "/home/ludacmfrif/www";//On line path
-if($_SERVER['HTTP_HOST'] == 'localhost'){
-  $www_real_path = "C:\wamp64\www\ludacm\www"; //For WAMP
-}
-else{
-include_once("model/security/htaccess_ok.php");
-}
-include_once('model/error/OvhErrorMessage.php');
-include_once("model/config/config.php"); 
 
-if (!isset($_GET['section']) OR $_GET['section'] == 'index')
-{
-    include_once("controller/accueil/index.php");//include controller/ludacm/index.php
+/** 2017/09/01 Version without if(server = wamp)
+*   This code is now in a file in model directory.
+*   Prettier an cleaner: better                 */
+
+include_once("model/path/real_path.php");
+include_once("model/security/htaccess_ok.php");
+include_once('model/error/OvhErrorMessage.php');
+include_once("model/config/config.php");
+
+if (!isset($_GET['section']) OR $_GET['section'] == 'index'){
+    include_once("controller/accueil/index.php");//include homepage controller
 }
 
 // Tools:
